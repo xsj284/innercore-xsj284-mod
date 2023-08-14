6046826be86abb3a646dfdd42ca5de1c88b6f6be
xsj284 <1302551371@qq.com>
2023-08-15 00:58:27 +0800

    rename

--- java/myicmod/src/com/xsj284/myicmod/Boot.java
+++ java/myicmod/src/com/xsj284/myicmod/Boot.java
@@ -1,4 +1,4 @@
-package com.xsj284.sample;
+package com.xsj284.myicmod;
 
 import com.zhekasmirnov.horizon.runtime.logger.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +8,5 @@ import java.util.HashMap;
 public class Boot {
     public static void boot(@NotNull HashMap<String, String> sources) {
         Logger.debug("TEST_MOD", sources.toString());
-
     }
 }
